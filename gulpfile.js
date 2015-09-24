435f2bdf35414fc5b6f6269250041fb4b6d26d56
Lucas Vieira <vieiralucas4@gmail.com>
2015-09-24 01:15:36 -0300

    Remove eslint, add jscs

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,63 @@
 'use strict';
+
 var path = require('path');
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 var coveralls = require('gulp-coveralls');
-
-gulp.task('static', function () {
-  return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+var jscs = require('gulp-jscs');
 var babel = require('gulp-babel');
 
-gulp.task('nsp', function (cb) {
-  nsp('package.json', cb);
+gulp.task('nsp', function(cb) {
+    nsp('package.json', cb);
 });
 
-gulp.task('pre-test', function () {
-  return gulp.src('lib\**\*.js')
-    .pipe(istanbul({
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire());
+gulp.task('pre-test', function() {
+    return gulp.src('lib\**\*.js')
+        .pipe(istanbul({
+            includeUntested: true
+        }))
+        .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
+gulp.task('test', ['pre-test'], function(cb) {
+    var mochaErr;
 
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
+    gulp.src('test/**/*.js')
+        .pipe(plumber())
+        .pipe(mocha({reporter: 'spec'}))
+        .on('error', function(err) {
+            mochaErr = err;
+        })
+        .pipe(istanbul.writeReports())
+        .on('end', function() {
+            cb(mochaErr);
+        });
 });
 
-gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
+gulp.task('coveralls', ['test'], function() {
+    if (!process.env.CI) {
+        return;
+    }
 
-gulp.task('babel', function () {
+    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+        .pipe(coveralls());
+});
+
+gulp.task('babel', function() {
     return gulp.src('src/**/*.js')
         .pipe(babel())
         .pipe(gulp.dest('lib'));
 });
 
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
+gulp.task('jscs', function() {
+    return gulp.src('**/*.js')
+        .pipe(excludeGitignore())
+        .pipe(jscs())
+        .pipe(jscs.reporter());
 });
 
-gulp.task('default', ['static', 'test', 'coveralls']);
-
-gulp.task('prepublish', ['test', 'nsp', 'babel']);
+gulp.task('prepublish', ['jscs', 'test', 'nsp', 'babel']);
--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
+// jscs:disable maximumLineLength
 'use strict';
 
 var assert = require('assert');
 var robotto = require('../lib');
 
-describe('robotto', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
+describe('robotto', function() {
+    it('should have unit test!', function() {
+        assert(false, 'we expected this package author to add actual unit tests.');
+    });
 });
