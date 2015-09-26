754faae5651a6b7606c388a34fed24b36aa1ddd4
lucasfcosta <fernandesdacostalucas@gmail.com>
2015-09-26 15:42:23 -0300

    parseRobots algorithm returning correct rules object

--- src/robotto.js
+++ src/robotto.js
@@ -5,18 +5,48 @@ function Robotto() {
 }
 
 Robotto.prototype.fetch = function(url, callback) {
-    request(url, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            let robotsRules = this.parseRobots(body);
-            callback(robotsRules);
-        }
-    });
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      let robotsRules = this.parseRobots(body);
+      callback(robotsRules);
+    }
+  });
 };
 
 Robotto.prototype.parseRobots = function(robotsFile) {
-    // TODO parse robots txt and return an object with rules
-    // Nice example to test: https://www.npmjs.com/robots.txt
-    // Rules about robots.txt: http://www.robotstxt.org/robotstxt.html
+  var lines = robotsFile.split('\n');
+  var rulesObj = {};
+
+  for (let i = 0; i < lines.length; i++) {
+    let result;
+
+    // If it finds an User-agent creates a new key into the rules object
+    if ((result = /^User-agent: (.*)/i.exec(lines[i])) !== null) {
+      Object.defineProperty(rulesObj, result[1], {
+        configurable: true,
+        writable: true,
+        enumerable: true,
+        value: {
+          allow: [],
+          disallow: []
+        }
+      });
+
+      // Look for Allowed Routes until it finds another user agent definition
+      var j = 1;
+      let permissionResult
+      while ((permissionResult = /^User-agent: (.*)/i.exec(lines[i+j])) === null && j < lines.length) {
+        if ((permissionResult = /^Allow: (.*)/i.exec(lines[i+j])) !== null) {
+          rulesObj[result[1]].allow.push(permissionResult[1]);
+        } if ((permissionResult = /^Disallow: (.*)/i.exec(lines[i+j])) !== null) {
+          rulesObj[result[1]].disallow.push(permissionResult[1]);
+        }
+        j++;
+      }
+    }
+  }
+
+  return rulesObj;
 };
 
 module.exports = Robotto;
