f1cdab72be30fcda012f2bf4684588e9482e1def
Lucas Vieira <vieiralucas4@gmail.com>
2015-09-27 03:40:12 -0300

    Implement _getRobotsUrl

--- src/robotto.js
+++ src/robotto.js
@@ -1,9 +1,13 @@
 'use strict';
+
 const request = require('request');
 const url = require('url');
 
 let robotto = {};
 
+robotto._getRobotsUrl = function(urlP) {
+    let receivedUrl = url.parse(urlP);
+    return `${receivedUrl.protocol}\/\/${receivedUrl.host}/robots.txt`;
 };
 
 
