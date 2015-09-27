7f41c873ca8c32ff7b47e2ff225305c381aa1fa4
Lucas Vieira <vieiralucas4@gmail.com>
2015-09-27 03:49:56 -0300

    Implement fetch

--- src/robotto.js
+++ src/robotto.js
@@ -10,9 +10,26 @@ robotto._getRobotsUrl = function(urlP) {
     return `${receivedUrl.protocol}\/\/${receivedUrl.host}/robots.txt`;
 };
 
+robotto._request = request;
 
+robotto.fetch = function(urlP, callback) {
+    callback = typeof callback === 'function' ? callback : new Function();
 
+    let robotsUrl = robotto._getRobotsUrl(urlP);
 
+    robotto._request(robotsUrl, (err, res, body) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (res.statusCode !== 200) {
+            callback(new Error(`Could not fetch robots.txt from ${urlP}, the server returned ${res.statusCode} code`));
+            return;
+        }
+
+        callback(null, body);
+    });
 };
 
 module.exports = robotto;
