737afa2ae6fe3259568bff2ecf386381960124ca
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-01-30 13:45:26 -0200

    Push commit and relevant tags together when publishing

--- gulpfile.js
+++ gulpfile.js
@@ -98,7 +98,7 @@ gulp.task('tag', ['bump'], () => {
         .pipe(git.add())
         .pipe(git.commit(`Release ${version}`, {args: '--allow-empty'}))
         .pipe(git.tag(version, `Release ${version}`, {args: '-a'}, (err) => {
-            if (!err) git.push('origin', 'master', {args: version});
+            if (!err) git.push('origin', 'master', {args: '--follow-tags'});
         }));
 });
 
